526834780342a4d0c55edaa277a658e50796926c
PhysTeach42 <119270116+PhysTeach42@users.noreply.github.com>
2025-01-12 11:46:00 -0600

    Update 2-try-async.js

--- 2-try-async.js
+++ 2-try-async.js
@@ -1,6 +1,6 @@
 // using async and await instead of chaining then()
 // still needs error handling with try..catch
-const url = 'https://jsonplaceholder.typicode.com/users';
+const url = 'https://jsonplaceholder.typicode.com/users000';
 
 export async function getData() {
   //
